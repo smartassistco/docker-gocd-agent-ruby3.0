9a6f0f8ccb775a7f9cb7fbf7b17636cf74d9b6c3
Stallon <stallon@live.com>
2021-11-27 22:03:05 +0530

    Updating GoCD agent

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ version: "3.8"
 
 services:
   gocd-agent-ruby3.0:
-    image: smartassist/gocd-agent-ruby3.0:v21.2.0
+    image: smartassist/gocd-agent-ruby3.0:v21.3.0
     restart: unless-stopped
     env_file: .env
 ```
@@ -25,13 +25,13 @@ services:
 - To modify further, reference in your Dockerfile:
 
 ```dockerfile
-FROM smartassist/gocd-agent-ruby3.0:v21.2.0
+FROM smartassist/gocd-agent-ruby3.0:v21.3.0
 ```
 
 ## Contents
 
 - [GoCD's official Debian 10 agent's Dockerfile](https://hub.docker.com/r/gocd/gocd-agent-debian-10)
-- [Ruby's official 3 Dockerfile](https://github.com/docker-library/ruby/raw/master/3/buster/Dockerfile)
+- [Docker's official Ruby 3.0 Dockerfile](https://github.com/docker-library/ruby/raw/master/3.0/buster/Dockerfile)
 - [Buildpack Dependencies](https://github.com/docker-library/buildpack-deps/raw/master/debian/buster/Dockerfile)
 
 ## Versions
@@ -39,5 +39,11 @@ FROM smartassist/gocd-agent-ruby3.0:v21.2.0
 | Runtime    | Version |
 |------------|---------|
 | OS      | Debian 10 (Buster)  |
-| GoCD agent | 21.2.0 |
-| Ruby       | 3.0.1  |
+| GoCD agent | 21.3.0 |
+| Ruby       | 3.0.3  |
+| Node       | 16.13.0  |
+
+## Building
+
+- Update versions in `generate.sh`
+- Run `bash generate.sh`
--- generate.sh
+++ generate.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-echo "Creating workspace"
+GOCD_AGENT_VERSION=21.3.0
+RUBY_VERSION=3.0
+NODE_VERSION=16
 
-mkdir -p tmp output
+echo "Creating temp directory"
+
+mkdir -p tmp
 
 echo "Downloading source files"
 
 wget -q -O tmp/curl.dockerfile "https://github.com/docker-library/buildpack-deps/raw/master/debian/buster/curl/Dockerfile"
 wget -q -O tmp/scm.dockerfile "https://github.com/docker-library/buildpack-deps/raw/master/debian/buster/scm/Dockerfile"
 wget -q -O tmp/buildpack-deps.dockerfile "https://github.com/docker-library/buildpack-deps/raw/master/debian/buster/Dockerfile"
-wget -q -O tmp/ruby.dockerfile "https://github.com/docker-library/ruby/raw/master/$RUBY_VERSION_MAJOR/buster/Dockerfile"
+wget -q -O tmp/ruby.dockerfile "https://github.com/docker-library/ruby/raw/master/$RUBY_VERSION/buster/Dockerfile"
+wget -q -O tmp/node.dockerfile "https://github.com/nodejs/docker-node/raw/main/$NODE_VERSION/buster/Dockerfile"
 
 echo "Creating Dockerfile"
 
@@ -30,15 +35,12 @@ echo "Creating Dockerfile"
   echo -e "\\n#\\n# Ruby\\n#\\n"
   sed "/^FROM.*/d; /^CMD.*/d" tmp/ruby.dockerfile
 
+  echo -e "\\n#\\n# Node\\n#\\n"
+  sed "/^FROM.*/d; /^CMD.*/d; /^COPY docker-entrypoint.sh.*/d; /^ENTRYPOINT.*/d; s/id[[:space:]]1000/id 1001/g" tmp/node.dockerfile
+
   echo -e "\\n# Change user back to go"
   echo "USER go"
 
-} >output/Dockerfile
+} >Dockerfile
 
 echo "Dockerfile generated"
-
-echo "Updating README"
-
-envsubst <templates/README.md >output/README.md
-
-echo "README generated"
